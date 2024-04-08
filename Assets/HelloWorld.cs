04b1bb399ff302fb77474e93e002c56193ed73bf
marcio-t <a53581@alunos.ipb.pt>
2024-04-08 12:43:30 +0100

    Edit the Update function

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,12 +7,12 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World");
+
     }
 }
