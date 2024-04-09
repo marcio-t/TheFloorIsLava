704a9cb781815043473c9d657e29a60624c4bb9a
marcio-t <a53581@alunos.ipb.pt>
2024-04-09 15:05:07 +0100

    Increment the scale

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string myMessage;
+
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
-        Debug.Log("Hello World");
+        Debug.Log(myMessage);
 
     }
 }
